56410e23e244fded6459f0b2d0162b5ad0c8ba6a
s4553711 <s4553711@gmail.com>
2015-09-06 23:40:44 +0800

    Read stand input from command line then output

--- socket-name-pipe/src/com/CK/util/Runner.java
+++ socket-name-pipe/src/com/CK/util/Runner.java
@@ -1,9 +1,30 @@
 package com.CK.util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Runner {
-	public static void main(String[] args) {
-		System.out.println("GO");
-		SocketNamePipe pipe = new SocketNamePipe();
-		pipe.attach();
-	}
+    public static void main(String[] args) {
+        System.out.println("GO");
+        SocketNamePipe pipe = new SocketNamePipe();
+        pipe.attach();
+
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(System.in));
+            String line;
+            while ((line = in.readLine()) != null) {
+                System.out.println("get > " + line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error");
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }

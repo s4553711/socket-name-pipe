c2e166906633db1dd923b601c60eae1e1dc1a012
s4553711 <s4553711@gmail.com>
2015-09-08 21:33:08 +0800

    Add example of a tcp server to fetch data from another tcp client

--- socket-name-pipe/src/com/CK/util/Runner.java
+++ socket-name-pipe/src/com/CK/util/Runner.java
@@ -1,30 +1,56 @@
 package com.CK.util;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class Runner {
+    private static Socket socket;
     public static void main(String[] args) {
         System.out.println("GO");
         SocketNamePipe pipe = new SocketNamePipe();
         pipe.attach();
 
-        BufferedReader in = null;
+        initTcpClient();
         try {
-            in = new BufferedReader(new InputStreamReader(System.in));
-            String line;
-            while ((line = in.readLine()) != null) {
-                System.out.println("get > " + line);
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            BufferedReader in = null;
+            try {
+                in = new BufferedReader(new InputStreamReader(System.in));
+                String line;
+                while ((line = in.readLine()) != null) {
+                    //System.out.println("get > " + line);
+                    out.writeBytes(line+"\n");
+                }
+                out.writeBytes("stopSignal");
+                System.out.println("stopSignal");
+            } catch (IOException e) {
+                System.out.println("Error");
+            } finally {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        } catch (IOException e) {
-            System.out.println("Error");
+            out.close();
+        } catch (IOException e1) {
+            e1.printStackTrace();
         } finally {
             try {
-                in.close();
+                socket.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+    private static void initTcpClient() {
+        try {
+            socket = new Socket("localhost", 45678);
+        } catch (Exception e) {
+            System.out.println("Initate tcp client falied! "+e);
         }
     }
 }
---
+++ socket-name-pipe/src/com/CK/util/TCPServer.java
@@ -0,0 +1,44 @@
+package com.CK.util;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer {
+    public static int port = 45678;
+    private static ServerSocket serv;
+    
+    public static void main(String args[]) throws Exception {
+        try {
+            serv = new ServerSocket(port, 4);
+        } catch (Exception e) {
+            System.out.println("Init ServerSocket Error"+e);
+        }
+        Socket s1 = null;
+        try {
+            System.out.println("Starting server...");
+            while (true) {
+                s1 = serv.accept();
+                BufferedReader fromClient = new BufferedReader(
+                        new BufferedReader(new InputStreamReader(s1.getInputStream()))
+                );
+                String str;
+                System.out.println("Wait for the text");
+                while((str = fromClient.readLine()) != null) {
+                    System.out.println("get from client > "+str);
+                    if (str.trim().equals("stopSignal")){
+                        break;
+                    }
+                }
+                fromClient.close();
+                System.out.println("Server down");
+                break;
+            }
+        } catch (Exception e) {    
+            System.out.println("Server setup error"+e);
+        } finally {            
+            serv.close();
+        }
+    }
+}

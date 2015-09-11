1132cfb1d19384e9a9791f15d3ef3b8b509641c5
s4553711 <s4553711@gmail.com>
2015-09-12 00:14:27 +0800

    The first release to use udp client and udp server

---
+++ socket-name-pipe/src/com/CK/util/UDPClient.java
@@ -0,0 +1,38 @@
+package com.CK.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class UDPClient {
+    public static void main(String args[]) throws Exception {
+        try {
+            String serverHostname = new String("127.0.0.1");
+            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+            DatagramSocket clientSocket = new DatagramSocket();
+
+            InetAddress IPAddress = InetAddress.getByName(serverHostname);
+            System.out.println("Attemping to connect to " + IPAddress + ") via UDP port 9876");
+
+            byte[] sendData = new byte[16384];
+//            byte[] receiveData = new byte[1024];
+
+            String sentence;
+            while((sentence = inFromUser.readLine()) != null) {
+                sendData = sentence.getBytes();
+                //System.out.println("Sending data to " + sendData.length + " bytes to server.");
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 45678);
+                clientSocket.send(sendPacket);                
+            }
+            clientSocket.close();
+        } catch (UnknownHostException ex) {
+            System.err.println(ex);
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
+}
--- socket-name-pipe/src/com/CK/util/UDPServer.java
+++ socket-name-pipe/src/com/CK/util/UDPServer.java
@@ -6,12 +6,12 @@ import java.net.DatagramSocket;
 public class UDPServer {
     public static void main(String args[]) throws Exception {
         DatagramSocket server = new DatagramSocket(45678);
-        byte[] receiveData = new byte[1024];
+        byte[] receiveData = new byte[16384];
         while(true) {
             DatagramPacket packet = new DatagramPacket(receiveData, receiveData.length);
             server.receive(packet);
             String str = new String(packet.getData());
-            System.out.println("str > "+str);
+            //System.out.println("str > "+str);
             if (str.equals("stopSignal")) {
                 server.close();
             }

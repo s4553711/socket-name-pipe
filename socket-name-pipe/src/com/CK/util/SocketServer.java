509629456eec4b084a812bce8ea9de2f00cd416a
s4553711 <s4553711@gmail.com>
2015-09-16 23:12:06 +0800

    Test code for custom made named pipe

--- socket-name-pipe/src/com/CK/util/SocketServer.java
+++ socket-name-pipe/src/com/CK/util/SocketServer.java
@@ -6,36 +6,52 @@ import java.io.RandomAccessFile;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class SocketServer {
     private DatagramSocket server;
     private RandomAccessFile pipe;
     private byte[] receiveData = new byte[16384];
+    private MemQueue out;
+    Thread t;
+    private BlockingQueue<byte[]> queue;
     
     public SocketServer(int port) {
         try {
             server = new DatagramSocket(port);
-            createNamedPipe();
+            //createNamedPipe();
+            queue = new LinkedBlockingQueue<>();
+            out = new MemQueue(queue);
+            t = new Thread(out);
+            System.out.println("go");
+            //out.run();
+            t.start();
+            System.out.println("finish");
         } catch (SocketException e) {
             e.printStackTrace();
         }
     }
     
     public void receive() {
+        System.out.println("start receive");
         while(true) {
             DatagramPacket packet = new DatagramPacket(receiveData, receiveData.length);
             try {
                 System.out.println("wait to receive ... ");
                 server.receive(packet);
                 System.out.println("after receive ...");
-                pipe.write(receiveData);
+                //pipe.write(receiveData);
+                queue.put(receiveData);
                 System.out.println("finish write ...");
                 String receive = new String(receiveData, 0, packet.getLength());
-                System.out.println("get > "+receive);
+                System.out.println("get > "+receive+" <<");
                 if (receive.equals("stopSignal")) {
                     System.out.println("Receive stop signal ...");
                     break;
                 }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -44,13 +60,15 @@ public class SocketServer {
     }
     
     private void close() {
-        try {
+        //try {
             server.close();
-            pipe.close();
-            new File("/tmp/test-pipe").delete();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            //out.terminate();
+            out.terminate();
+            //pipe.close();
+            //new File("/tmp/test-pipe").delete();
+        //} catch (IOException e) {
+        //    e.printStackTrace();
+        //}
     }
     
     private void createNamedPipe() {

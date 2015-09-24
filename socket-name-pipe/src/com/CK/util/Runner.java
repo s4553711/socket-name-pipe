d9ab1b15ca18097eeb8540903d36ddbace6af995
s4553711 <s4553711@gmail.com>
2015-09-24 23:07:24 +0800

    Pass port through argument

--- socket-name-pipe/src/com/CK/util/Runner.java
+++ socket-name-pipe/src/com/CK/util/Runner.java
@@ -5,8 +5,7 @@ import java.io.InputStream;
 
 public class Runner {
     public static void main(String[] args) {
-        //SocketNamePipe pipe = new SocketNamePipe("localhost", 45678);
-        TCPNamedPipe pipe = new TCPNamedPipe("localhost", 45678);
+        TCPNamedPipe pipe = new TCPNamedPipe("localhost", Integer.valueOf(args[0]));
         try {
             int nRead;
             byte[] data = new byte[8192];
--- socket-name-pipe/src/com/CK/util/TCPServer.java
+++ socket-name-pipe/src/com/CK/util/TCPServer.java
@@ -4,7 +4,7 @@ public class TCPServer {
 
     public static void main(String args[]) {
         System.out.println("TCP Server start");
-        TCPSocketServer server = new TCPSocketServer(45678);
+        TCPSocketServer server = new TCPSocketServer(Integer.valueOf(args[0]));
         server.receive();
         System.out.println("Server shutdown");
     }

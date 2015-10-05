4f2a5d34a5ed96c7e0aed6a0f3d5c4be54f73b0f
s4553711 <s4553711@gmail.com>
2015-10-05 23:02:02 +0800

    add a example for paired-end fastq reader

---
+++ socket-name-pipe/example/pipe.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use utf8;
+use Data::Dumper;
+
+my $port = shift;
+my $pair1 = shift;
+my $pair2 = shift;
+
+my $pid = 0;
+$pid = fork();
+if ($pid == 0) {
+	fastqReader($pair1, $port);
+	exit;
+}
+
+my $pid2 = 0;
+$pid2 = fork();
+if ($pid2 == 0) {
+	fastqReader($pair2, $port+1);
+	exit;
+}
+
+sub fastqReader {
+	my ($file, $port) = @_;
+	my $ifh = undef;
+	my $command = "zcat $file | head -n 1200 | java -cp bin/src com.CK.util.Runner $port ".
+		";echo -n 'stopSignal' | java -cp bin/src com.CK.util.Runner $port |";
+	#print ">> $command\n";
+	open ($ifh, $command);
+	while (my $line = <$ifh>) {
+		print $line;
+	}
+	close $ifh;
+}

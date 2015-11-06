21a43e72df8fc5b0069831d492bbe9510eba777a
s4553711 <s4553711@gmail.com>
2015-11-06 23:11:22 +0800

    1. Remove unused code and some comment. 2. Record start and end time while receiving data.

--- socket-name-pipe/src/com/CK/util/MemQueue.java
+++ socket-name-pipe/src/com/CK/util/MemQueue.java
@@ -1,7 +1,5 @@
 package com.CK.util;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.concurrent.BlockingQueue;
 
 public class MemQueue implements Runnable {
@@ -19,31 +17,18 @@ public class MemQueue implements Runnable {
     public void run() {
         try {
             int i = 0;
-            //System.out.println("runnable");
-            PrintWriter writer = new PrintWriter("./a.txt", "UTF-8");
             while(true) {
                 while (!queue.isEmpty()) {
                     i++;
                     String takeout = new String(this.queue.take());
-                    //System.out.println(">>>");
                     System.out.print(takeout);
-                    //System.out.println("<<<");
-                    //System.out.println("size> "+i+" with "+queue.size()+" "+takeout.length());
-                    writer.print(takeout);
                 }
-                //System.out.println("empty");
                 if (!isRunning) {
-                    //System.out.println("clear and leave the thread");
                     break;
                 }
                 Thread.sleep(1000);
             }
-            writer.close();
-            //System.out.println("thread begin to done");
-        } catch (IOException e) {
-            e.printStackTrace();
         } catch (InterruptedException e) {
-            //System.out.println("get exception");
             try {
                 if (!this.queue.isEmpty()) {
                     System.out.print(new String(this.queue.take()));
@@ -53,7 +38,6 @@ public class MemQueue implements Runnable {
             }
             //System.out.println("Error while take out contetn from blocking queue");
         }
-        //System.out.println("Thread end");
     }
 
     public void put(byte[] realPack) throws InterruptedException {
--- socket-name-pipe/src/com/CK/util/TCPSocketServer.java
+++ socket-name-pipe/src/com/CK/util/TCPSocketServer.java
@@ -2,37 +2,43 @@ package com.CK.util;
 
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class TCPSocketServer {
     private ServerSocket server;
     private byte[] receiveData = new byte[8192];
     private MemQueue mem;
-    //private FileBasedPipe fmem;
     Thread t;
+    private PrintWriter writer;
     
     public TCPSocketServer(int port) {
         try {
             server = new ServerSocket(port);
+            writer = new PrintWriter("socketServer-"+port+".log", "UTF-8");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //fmem = new FileBasedPipe("/tmp/testpipe");
         mem = new MemQueue(new LinkedBlockingQueue<byte[]>());
         t = new Thread(mem);
-        t.start();          
+        t.start();
     }
     
     public void receive() {
-        //System.out.println("start receive");
         Socket s1;
+        boolean firstSignal = true;
         while(true) {
             try {
                 s1 = server.accept();
-                //System.out.println("accept");
+                if (firstSignal) {
+                    writer.println("start > "+new SimpleDateFormat("yyyyMMdd_HHmmss.SSS").format(Calendar.getInstance().getTime()));
+                    firstSignal = false;
+                }
                 DataInputStream in = new DataInputStream(s1.getInputStream());
                 int bytesRead = 0;
                 boolean end = false;
@@ -44,16 +50,12 @@ public class TCPSocketServer {
                     } else {
                         byte[] realPack = Arrays.copyOfRange(receiveData, 0, bytesRead);
                         receive = new String(realPack).trim();
-                        //System.out.println("read .. "+bytesRead+", really size:"+receiveData.length);
-                        //System.out.println("put .. \n"+receive); 
                         if (!receive.equals("stopSignal")) {
                             mem.put(realPack);
-                            //fmem.put(realPack);
                         }
                     }
                 }
                 if (receive.equals("stopSignal")) {
-                    //System.out.println("Receive stop signal ...");
                     break;
                 }
                 receive = "";
@@ -68,15 +70,16 @@ public class TCPSocketServer {
     
     private void close() {
         try {
+            writer.flush();
+            writer.println("end > "+new SimpleDateFormat("yyyyMMdd_HHmmss.SSS").format(Calendar.getInstance().getTime()));
+            writer.close();
             server.close();
             mem.terminate();
-            //fmem.terminate();
             t.join();
         } catch (InterruptedException e) {
-            //System.out.println("close thread error");
             t.interrupt();
         } catch (IOException e) {
-            //System.out.println("TCP server close error");
+            e.printStackTrace();
         }
     }
 }

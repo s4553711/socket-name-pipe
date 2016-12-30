5c0851971770c741b170a12b37e326b48569ce39
CK, Lee <s4553711@gmail.com>
2016-12-31 00:28:46 +0800

    Get init Receiver setting from config file for SamDispatch

--- socket-name-pipe/src/com/CK/run/SamDispatch.java
+++ socket-name-pipe/src/com/CK/run/SamDispatch.java
@@ -12,15 +12,19 @@ import com.CK.util.TCPNamedPipe;
 public class SamDispatch {
 	public static void main(String[] args) {
 
-		Map<String, TCPNamedPipe> maps = init_sys();	
+		Map<String, TCPNamedPipe> maps = init_sys(args[0]);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String line;
 		try {
 			while((line = br.readLine()) != null) {
-				String[] col = line.split("\t");
-				byte[] data = line.concat("\n").getBytes();
-				maps.get(col[2].replace("chr", "")+"-"+(int) Math.floor(Integer.valueOf(col[3])/10000000))
-					.push(data, data.length);
+				if (!line.startsWith("@")) {
+					String[] col = line.split("\t");
+					byte[] data = line.concat("\n").getBytes();
+					int index = (int)Math.floor(Integer.valueOf(col[3])/249250621);
+					String chrI = col[2].replace("chr", "");
+					//System.out.println("send > "+chrI+"-"+index+"("+col[2]+","+col[3]+")");
+					maps.get(chrI+"-"+index).push(data, data.length);
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -30,13 +34,12 @@ public class SamDispatch {
 		}
 	}
 
-	public static Map<String, TCPNamedPipe> init_sys() {
+	public static Map<String, TCPNamedPipe> init_sys(String config) {
 		int start_port = 4000, i = 0;
 		Map<String, TCPNamedPipe> maps = new HashMap<>();
 		BufferedReader br = null;
 		try {
-			br = new BufferedReader(
-				new FileReader("/home/s4553711/git/socket-name-pipe/socket-name-pipe/nodes.config.2"));
+			br = new BufferedReader(new FileReader(config));
 			String line;
 			while((line = br.readLine()) != null) {
 				if (line.startsWith("Interval") || line.startsWith("Chr")) {

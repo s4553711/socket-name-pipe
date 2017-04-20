d333b91e04d72284ca736f5bd9b47bc2d04c36a3
CK, Lee <s4553711@gmail.com>
2017-04-20 23:25:20 +0800

    add config to sam dispatch example

--- socket-name-pipe/script/send.sh
+++ socket-name-pipe/script/send.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+config=$1
 
 rm *.time.log
 for i in $(seq 1 1); do
-	/usr/bin/time -v -o $i.time.log ~/git/samtools/samtools view -hS /mnt/repo1/backup/bio/alignment/wgEncodeUwRepliSeqBg02esG1bAlnRep1.bam  | java -cp build/libs/socket-name-pipe.jar com.CK.run.SamDispatch 2003 &
+	/usr/bin/time -v -o $i.time.log ~/git/samtools/samtools view -hS /mnt/repo1/backup/bio/alignment/wgEncodeUwRepliSeqBg02esG1bAlnRep1.bam  | java -cp build/libs/socket-name-pipe.jar com.CK.run.SamDispatch $config &
 	#/usr/bin/time -v -o $i.time.log cat /dev/shm/a.sam  | java -cp build/libs/socket-name-pipe.jar com.CK.run.SamDispatch 2003 &
 done
 wait

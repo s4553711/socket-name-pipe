8b3782c53ecd753615f08c6bfb33865e7eafae80
s4553711 <s4553711@gmail.com>
2017-06-14 11:18:00 +0800

    Providing header for TCPServer

--- socket-name-pipe/src/com/CK/util/TCPServer.java
+++ socket-name-pipe/src/com/CK/util/TCPServer.java
@@ -1,8 +1,32 @@
 package com.CK.util;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TCPServer {
 
+    public static void readHeader() {
+    	BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader("header.sam"));
+			String line;
+			while((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+    }
+	
     public static void main(String args[]) {
+    	readHeader();
         TCPSocketServer server = new TCPSocketServer(Integer.valueOf(args[0]));
         server.receive();
     }
